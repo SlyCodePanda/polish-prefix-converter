79c55ac9d5e62080b6ec4a1e0c06fbb3870c5b9f
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-12 14:39:17 +0000

    start

---
+++ infix.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class infix
+{
+public:
+	// Characters that represent the operators of a statement.
+	std::string plus = "+";
+	//std::string times = "*";
+	//std::string div = "/";
+	//std::string min = "-";
+
+	// The function that converts the prefix statement to infix.
+	std::vector<int> convert( std::vector<std::string> input );
+
+	infix();
+	~infix();
+};
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+#include <string>
+#include <sstream>
+
+#include "infix.h"
+
+int main()
+{
+    std::vector<std::string> input;
+    std::string line;
+
+    // Takes in the Polish Prefix statement from user as a string to be fed to infix function.
+    if ( getline(std::cin, line) )
+   {
+      std::istringstream str(line);
+
+      std::string value;
+      str >> value;
+      input.push_back( value );
+      while ( str >> value ) 
+      {
+        input.push_back( value );
+      }
+   }
+    
+    // Creating infix object.
+    infix calc;
+    infix *ptrCalc = &calc;
+
+    std::vector<int> vec = ptrCalc->convert( input );
+
+    /*-----OUTPUT-----*/
+
+    // Printing out Polish Prefix input given by user.
+    std::cout << "inputted statement: " << std::endl;
+    for( int i = 0 ; i < input.size() ; i++ )
+    {
+        std::cout << input.at(i) << " ";
+    }
+    std::cout << "\n";
+
+    //std::cout << vec.at(0) << std::endl;
+
+    return 0;
+}
