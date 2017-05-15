0283566b56a5b480b5ab40352efa51417e4bc9b8
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-15 00:55:05 +0000

    fully functioning polish prefix calculator

--- infix.cpp
+++ infix.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <string.h>
 
+#include <typeinfo>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,40 +19,99 @@ std::vector<int> infix::convert( std::vector<std::string> input )
 {
 	int length = input.size();
 	std::vector<int> vec;
-/*
-	std::vector<int> value1;
 
-	std::cout << length << std::endl;
-	for ( int i = 0 ; i < length ; i++ )
-	{
-		if ( input.at(i) != plus )
-			std::cout << "These aren't plus signs! " << input.at(i) << "\n";
-			value1.push_back( atoi( input.at(i)) );
-	}
-	std::cout << "\n";
-*/
+	// testing input vector...
+	std::cout << "input length: " << length << "\n";
+
 	// Scans the given prefix expression from right to left.
-	for( int i = length ; i >= 0 ; i-- )
+	for( int i = length-1 ; i >= 0 ; i-- )
 	{
+		// For test, checking input vector.
 		std::cout << input.at(i) << std::endl;
-		if( input.at(i) != plus )
+
+		// If 'operand' (i.e nonnegative number between 0 - 99) ...
+		// If element is not an operator.
+		if( input.at(i) != plus && input.at(i) != times && input.at(i) != divi && input.at(i) != min )
 		{
-			//std::cout << "found a number! Adding to vector. \n";
+			// Convert the found element into an integer 'x' and push it into vector.
 			std::string x = input.at(i);
-			std::stoi(x);
-			vec.push_back( x );
+			int newX = std::stoi(x);
+
+			//vec.push_back( newX );
+			vec.insert(vec.begin(), newX);
+
+			// Testing to see if all elements are being added correctly.
+			std::cout << "Operand deteceted: \n";
+			for( int i = 0 ; i < vec.size() ; i++ )
+			{
+				std::cout << vec.at(i) << " ";
+			}
+			std::cout << "\n";
 		}
+		//  If 'operator' (i.e +, -, *, /) ...
+		// If operator is a '+'.
 		if( input.at(i) == plus )
 		{
-			int opperand1 = (vec.at(0)) - '0';
-			int opperand2 = (vec.at(1)) - '0';
-			vec.pop_back();
-			vec.pop_back();
+			// In the case of a '+' being found, use said operator on the two operands in vector.
+			// Then push result onto stack.
+			int opperand1 = (vec.at(0));
+			int opperand2 = (vec.at(1));
 
+			vec.erase (vec.begin(),vec.begin()+2);
+
+			// Showing output of the computation being performed.
+			std::cout << opperand1 << " + " << opperand2 << "\n";
 			int operation = opperand1 + opperand2;
 			vec.push_back( operation );
 		}
+		// If operator is a '*'.
+		if( input.at(i) == times )
+		{
+			int opperand1 = (vec.at(0));
+			int opperand2 = (vec.at(1));
+
+			vec.erase (vec.begin(),vec.begin()+2);
+
+			// Showing output of the computation being performed.
+			std::cout << opperand1 << " * " << opperand2 << "\n";
+			int operation = opperand1 * opperand2;
+			vec.push_back( operation );
+		}
+		// If operator is a '/'.
+		if( input.at(i) == divi )
+		{
+			int opperand1 = (vec.at(0));
+			int opperand2 = (vec.at(1));
+
+			vec.erase (vec.begin(),vec.begin()+2);
+
+			// Showing output of the computation being performed.
+			std::cout << opperand1 << " / " << opperand2 << "\n";
+			int operation = opperand1 / opperand2;
+			vec.push_back( operation );
+		}
+		// If operator is a '-'.
+		if( input.at(i) == min )
+		{
+			int opperand1 = (vec.at(0));
+			int opperand2 = (vec.at(1));
+
+			vec.erase (vec.begin(),vec.begin()+2);
+
+			// Showing output of the computation being performed.
+			std::cout << opperand1 << " - " << opperand2 << "\n";
+			int operation = opperand1 - opperand2;
+			vec.push_back( operation );
+		}
 	}
+
+	// Testing to see vec contents.
+	std::cout << "vec: \n";
+	for( int i = 0 ; i < vec.size() ; i++ )
+	{
+		std::cout << vec.at(i) << " ";
+	}
+	std::cout << "\n";
 
 	return vec;
 }
--- infix.h
+++ infix.h
@@ -7,9 +7,9 @@ class infix
 public:
 	// Characters that represent the operators of a statement.
 	std::string plus = "+";
-	//std::string times = "*";
-	//std::string div = "/";
-	//std::string min = "-";
+	std::string times = "*";
+	std::string divi = "/";
+	std::string min = "-";
 
 	// The function that converts the prefix statement to infix.
 	std::vector<int> convert( std::vector<std::string> input );
